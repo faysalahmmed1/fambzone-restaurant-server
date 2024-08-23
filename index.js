396f53ccac7d0f2c952c98e64d8fa022bd1538a7
faysalahmmed1 <faysalahmmedcse@gmail.com>
2024-08-23 12:40:48 +0600

    create update menu items

--- index.js
+++ index.js
@@ -131,13 +131,37 @@ async function run() {
             res.send(MenuResult);
         });
 
+        app.get('/menu/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await MenuCollection.findOne(query);
+            res.send(result);
+        });
+
         app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
             const item = req.body;
             const result = await MenuCollection.insertOne(item);
             res.send(result);
         });
+        app.patch('/menu/:id', async (req, res) => {
+            const item = req.body;
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    name: item.name,
+                    category: item.category,
+                    price: item.price,
+                    recipe: item.recipe,
+                    image: item.image
+                }
+            }
+            const result = await MenuCollection.updateOne(filter, updateDoc);
+            res.send(result);
 
-        app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => { 
+        })
+
+        app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await MenuCollection.deleteOne(query);
